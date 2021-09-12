fe9c16a8a1e8d9313b5a407aec382834a3d41d6b
Alexander-Ploskin <ploskin0107@gmail.com>
2021-09-12 13:58:55 +0300

    Make first task

---
+++ Lab1/Program.cs
@@ -0,0 +1,64 @@
+﻿using MyMathLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab1
+{
+    public class Program
+    {
+        static IEnumerable<string> Introduce()
+        {
+            yield return "Labaratory work 1";
+            yield return "Numerical methods for non-linear equations";
+            yield return "Equation is 10 * cos(x) - 0,1 * x ^ 2 = 0";
+
+        }
+
+        static int EnterIntegerParameter(Predicate<int> predicate)
+        {
+            var result = 0;
+            while(!int.TryParse(Console.ReadLine(), out result) || !predicate(result))
+            {
+                Console.WriteLine("Enter correct value");
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            foreach(var message in Introduce())
+            {
+                Console.WriteLine(message);
+            }
+            Console.WriteLine("Enter amount of chunks to detach roots");
+            var chunksCount = EnterIntegerParameter(x => x > 0);
+
+            var func = new Func<double, double>(x => 10 * Math.Cos(x) - 0.1 * x * x);
+            var derivative = new Func<double, double>(x => -10 * Math.Sin(x) - 0.2 * x);
+            var section = (-8, 2);
+            var accuracy = 1.0e-5;
+
+            var splittedSection = NonLinearEquations.Split(func, section, chunksCount).ToList();
+            Console.WriteLine();
+            Console.WriteLine($"Found {splittedSection.Count} sections where function changes its sign");
+            splittedSection.ForEach(s => Console.WriteLine($"[{s.a}; {s.b}]"));
+
+            foreach (var method in Enum.GetValues(typeof(NonLinearEquations.Method)).Cast<NonLinearEquations.Method>())
+            {
+                Console.WriteLine();
+                Console.WriteLine(method.ToString() + "\n");
+
+                splittedSection.Select(s => NonLinearEquations.FindRoot(func, s, accuracy, method, derivative))
+                    .ToList().ForEach(result => {
+                    Console.WriteLine();
+                    Console.WriteLine($"Initial approximation is {result.InitialApproximation}");
+                    Console.WriteLine($"Length of last sector is {result.LastSectorLength}");
+                    Console.WriteLine($"Approximated root is {result.Root}");
+                    Console.WriteLine($"Absolute value for residual is {result.Residual}");
+                    Console.WriteLine($"Root was found in {result.StepsCount} steps");
+                });
+            }
+        }
+    }
+}
---
+++ MyMathLib/NonLinearEquations.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMathLib
+{
+    public class MethodResult
+    {
+        public MethodResult(double initialApproximation, int stepsCount, double root, double lastSecorLength, double residual)
+        {
+            InitialApproximation = initialApproximation;
+            StepsCount = stepsCount;
+            Root = root;
+            LastSectorLength = lastSecorLength;
+            Residual = residual;
+        }
+
+        public double InitialApproximation { get; }
+        public int StepsCount { get; }
+        public double Root { get; }
+        public double LastSectorLength { get; }
+        public double Residual { get; }
+    }
+
+    public static class NonLinearEquations
+    {
+        public enum Method
+        {
+            BisectionMetod, NewtonMethod, ModifiedNewtonMethod, SecantMethod
+        }
+
+        public static MethodResult FindRoot(Func<double, double> func,
+            (double a, double b) section,
+            double accuracy,
+            Method method,
+            Func<double, double> derivative = null)
+        {
+            if(section.b < section.a)
+            {
+                throw new ArgumentException("High border shouldn't be less than low border");
+            }
+
+            switch (method)
+            {
+                case Method.BisectionMetod:
+                    return BisectionMethod(func, section, accuracy);
+                case Method.NewtonMethod:
+                    return NewtonMethod(func, derivative, section, accuracy);
+                case Method.ModifiedNewtonMethod:
+                    return ModifiedNewtonMethod(func, derivative, section, accuracy);
+                case Method.SecantMethod:
+                    return SecantMethod(func, section, accuracy);
+                default:
+                    throw new ArgumentException("Unknown method");
+            }
+        }
+
+        public static IEnumerable<(double a, double b)> Split(Func<double, double> func, (double a, double b) section, int numberOfChunks)
+        {
+            var chunkLength = (section.b - section.a) / numberOfChunks;
+            for(double a = section.a, b = a + chunkLength; a < section.b; a += chunkLength, b += chunkLength)
+            {
+                if(func(a) * func(b) <= 0)
+                {
+                    yield return (a, b);
+                }
+            }
+        }
+
+        #region BisectionMethod
+        static (double a, double b) BisectionMethodCore(Func<double, double> func, (double a, double b) section, double accuracy, ref int counter)
+        {
+            counter++;
+            var c = (section.a + section.b) / 2;
+            return section.b - section.a < 2 * accuracy
+                ? section
+                : BisectionMethodCore(func, func(section.a) * func(c) <= 0 ? (section.a, c) : (c, section.b), accuracy, ref counter);
+        }
+
+        static MethodResult BisectionMethod(Func<double, double> func, (double a, double b) section, double accuracy)
+        {
+            var counter = 0;
+            var (a, b) = BisectionMethodCore(func, section, accuracy, ref counter);
+            return new MethodResult((section.a + section.b) / 2, counter, (a + b) / 2, b - a, Math.Abs(func((a + b) / 2)));
+        }
+        #endregion
+
+        #region NewtonMethod
+        static (double x, double nextX) NewtonMethodCore(Func<double, double> func, Func<double, double> derivative, double x, double accuracy, ref int counter, int p = 1)
+        {
+            counter++;
+            var nextX = x - func(x) * p / derivative(x);
+            return Math.Abs(x - nextX) > accuracy ? NewtonMethodCore(func, derivative, nextX, accuracy, ref counter, p) : (x, nextX);
+        }
+
+        static MethodResult NewtonMethod(Func<double, double> func, Func<double, double> derivative, (double a, double b) section, double accuracy)
+        {
+            if (derivative == null)
+            {
+                throw new ArgumentException("Specify derivatice to use Newthon method");
+            }
+            var counter = 0;
+            var (x, nextX) = NewtonMethodCore(func, derivative, derivative(section.a) != 0 ? section.a : section.a - accuracy, accuracy, ref counter);
+            return new MethodResult(section.a, counter, nextX, Math.Abs(x - nextX), Math.Abs(func(nextX)));
+        }
+        #endregion
+
+        #region ModifiedNewtonMethod
+        static MethodResult ModifiedNewtonMethod(Func<double, double> func, Func<double, double> derivative, (double a, double b) section, double accuracy)
+        {
+            if (derivative == null)
+            {
+                throw new ArgumentException("Specify derivatice to use Newthon method");
+            }
+            var counter = 0;
+            var (x, nextX) = NewtonMethodCore(func, x => derivative(section.a), derivative(section.a) != 0 ? section.a : section.a + accuracy, accuracy, ref counter);
+            return new MethodResult(section.a, counter, nextX, Math.Abs(x - nextX), Math.Abs(func(nextX)));
+        }
+        #endregion
+
+        #region SecantMethod
+        static (double x, double nextX) SecantMethodCore(Func<double, double> func, double x, double nextX, double accuracy, ref int counter)
+        {
+            counter++;
+            return Math.Abs(x - nextX) > accuracy
+                ? SecantMethodCore(func, nextX, x - func(x) * (nextX - x) / (func(nextX) - func(x)), accuracy, ref counter)
+                : (x, nextX);
+        }
+
+
+        static MethodResult SecantMethod(Func<double, double> func, (double a, double b) section, double accuracy)
+        {
+            var counter = 0;
+            var (x, nextX) = SecantMethodCore(func, section.a, section.b, accuracy, ref counter);
+            return new MethodResult(section.a, counter, nextX, Math.Abs(x - nextX), Math.Abs(func(nextX)));
+        } 
+        #endregion
+    }
+}
